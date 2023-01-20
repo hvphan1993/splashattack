43dc926a26b0fc0595dabcc998b3afbb7b23f9f2
hvphan1993 <hvphan1993@gmail.com>
2023-01-19 18:34:18 -0800

    added colors to game!

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ class Enemy {
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, "blue");
+const player = new Player(x, y, 10, "white");
 player.draw();
 
 // for future if enable player to move here is where to modify where projectiles start from
@@ -95,7 +95,7 @@ function spawnEnemies() {
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
     }
 
-    const color = "green";
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
 
     const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
 
@@ -111,10 +111,20 @@ function spawnEnemies() {
 let animationId
 function animate() {
   animationId = requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
-  projectiles.forEach((projectile) => {
+  projectiles.forEach((projectile, index) => {
     projectile.update();
+
+    // remove projectiles from edge of screen
+    if (projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width || projectile.y + projectile.radius < 0 || projectile.y - projectile.radius > canvas.height) {
+        setTimeout(() => {
+            projectiles.splice(index, 1)
+
+        }, 0)
+    }
+
   });
 
   enemies.forEach((enemy, index) => {
@@ -134,7 +144,7 @@ function animate() {
       if (dist - enemy.radius - projectile.radius < 1) {
         setTimeout(() => {
           enemies.splice(index, 1);
-          projectiles.splice(index, 1);
+          projectiles.splice(projectileIndex, 1);
         }, 0);
       }
     });
@@ -148,12 +158,12 @@ addEventListener("click", (event) => {
   );
 
   const velocity = {
-    x: Math.cos(angle),
-    y: Math.sin(angle),
+    x: Math.cos(angle) * 5,
+    y: Math.sin(angle) * 5,
   };
 
   projectiles.push(
-    new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, "white", velocity)
   );
 });
 
