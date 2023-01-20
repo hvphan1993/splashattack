01daf05aef420ec83c12ab9c2514e93856c8d656
hvphan1993 <hvphan1993@gmail.com>
2023-01-19 18:10:36 -0800

    added collision detection

--- index.js
+++ index.js
@@ -83,45 +83,62 @@ function spawnEnemies() {
   setInterval(() => {
     const radius = Math.random() * (30 - 4) + 4;
 
-    let x 
-    let y
+    let x;
+    let y;
 
     if (Math.random() < 0.5) {
-        x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
-        y = Math.random() * canvas.height;
-        // y = Math.random() < 0.5 ? 0- radius : canvas.height + radius;
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+      // y = Math.random() < 0.5 ? 0- radius : canvas.height + radius;
     } else {
-        x = Math.random() * canvas.width;
-        y = Math.random() < 0.5 ? 0- radius : canvas.height + radius;
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
     }
-  
+
     const color = "green";
 
-    const angle = Math.atan2(
-        canvas.height / 2 - y,
-        canvas.width / 2 - x
-      );
-    
-      const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle),
-      };
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
 
     enemies.push(new Enemy(x, y, radius, color, velocity));
   }, 1000);
 }
 
+let animationId
 function animate() {
-  requestAnimationFrame(animate);
+  animationId = requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
   });
-  
-  enemies.forEach((enemy) => {
-    enemy.update()
-  })
+
+  enemies.forEach((enemy, index) => {
+    enemy.update();
+
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+    // end game
+    if (dist - enemy.radius - player.radius < 1) {
+        cancelAnimationFrame(animationId)
+    }
+
+    projectiles.forEach((projectile, projectileIndex) => {
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+
+      // objects touch
+      if (dist - enemy.radius - projectile.radius < 1) {
+        setTimeout(() => {
+          enemies.splice(index, 1);
+          projectiles.splice(index, 1);
+        }, 0);
+      }
+    });
+  });
 }
 
 addEventListener("click", (event) => {
